b3bd4f7814aa52b330dacf912c873dfe5b672102
Andrej Palicka <andrej.palicka@gmail.com>
2026-05-25 18:17:53 +0200

    feat(yoke-tauri): scaffold Tauri 2 shell crate

---
+++ crates/yoke-tauri/src/main.rs
@@ -0,0 +1,12 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+fn main() {
+    tracing_subscriber::fmt()
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .init();
+
+    tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
